5027d79aa9136662a081ed9251977c6f615dcaaa
Huyf1995 <spyssor@gmail.com>
2019-01-23 19:44:20 +0800

    Complete Alipay module

---
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -0,0 +1,104 @@
+package com.mmall.controller.portal;
+
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.demo.trade.config.Configs;
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IOrderService;
+import com.mmall.service.impl.OrderServiceImpl;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/order/")
+public class OrderController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
+
+    @Autowired
+    private IOrderService iOrderService;
+
+
+    @RequestMapping("pay.do")
+    @ResponseBody
+    public ServerResponse pay(HttpSession session, Long orderNo, HttpServletRequest request){
+
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEGAL_ARGUMENT.getCode(), ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+        }
+
+        String path = request.getSession().getServletContext().getRealPath("upload");
+        return iOrderService.pay(orderNo, user.getId(), path);
+    }
+
+    @RequestMapping("alipay_callback.do")
+    @ResponseBody
+    public Object alipayCallBack(HttpServletRequest request){
+
+        Map<String, String> params = new HashMap<>();
+
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext();){
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i ++){
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+        logger.info("支付宝回调，sign{}, trade_status:{}, 参数:{}", params.get("sign"), params.get("trade_status"), params.toString());
+
+        //验证回调的正确性，判断是不是支付宝发的，并且避免重复通知
+        params.remove("sign_type");
+        try {
+            boolean alipayRSACheckedV2 = AlipaySignature.rsaCheckV2(params, Configs.getAlipayPublicKey(), "utf-8", Configs.getSignType());
+            if (!alipayRSACheckedV2){
+                return ServerResponse.createByErrorMessage("非法请求，验证不通过，再恶意请求我就报警了");
+            }
+        } catch (AlipayApiException e) {
+            logger.error("支付宝回调异常", e);
+        }
+
+        // 后台验证各种数据
+
+        //
+        ServerResponse serverResponse = iOrderService.aliCallback(params);
+        if (serverResponse.isSuccess()){
+            return Const.AlipayCallback.RESPONSE_SUCCESS;
+        }
+        return Const.AlipayCallback.RESPONSE_FAILED;
+
+    }
+
+    @RequestMapping("query_order_pay_status.do")
+    @ResponseBody
+    public ServerResponse<Boolean> queryOrderPayStatus(HttpSession session, Long orderNo){
+
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEGAL_ARGUMENT.getCode(), ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+        }
+
+        ServerResponse<Boolean> serverResponse = iOrderService.queryOrderPayStatus(user.getId(), orderNo);
+        if (serverResponse.isSuccess()){
+            return ServerResponse.createBySuccess(true);
+        }
+        return ServerResponse.createBySuccess(false);
+    }
+
+}
---
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -0,0 +1,14 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+
+import java.util.Map;
+
+public interface IOrderService {
+
+    ServerResponse pay(Long orderNo, Integer userId, String path);
+
+    public ServerResponse aliCallback(Map<String, String> params);
+
+    ServerResponse<Boolean> queryOrderPayStatus(Integer userid, Long orderNo);
+}
