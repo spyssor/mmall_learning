b4fac8d08e435c759e1db2a3ec3a88bc942c1db2
huyf <spyssor@gmail.com>
2019-02-13 17:52:25 +0800

    commit cookie

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -30,10 +30,17 @@ public class CookieUtil {
         return null;
     }
 
+    //X: .happymmall.com            cookie:domain= .happymmall.com;path="/"
+    //a:A.happymall.com             cookie:domain=A.happymmall.com;path="/"
+    //b:B.happymall.com             cookie:domain=B.happymmall.com;path="/"
+    //c:A.happymall.com/test/cc     cookie:domain=A.happymmall.com;path="/test/cc"
+    //d:A.happymall.com/test/dd     cookie:domain=A.happymmall.com;path="/test/dd"
+    //e:A.happymall.com/test        cookie:domain=A.happymmall.com;path="/test"
     public static void writeLoginToken(HttpServletResponse response, String token){
         Cookie ck = new Cookie(COOKIE_NAME, token);
         ck.setDomain(COOKIE_DOMAIN);
         ck.setPath("/");//代表设置在根目录，所有页面都能获取cookie
+        ck.setHttpOnly(true);
         ck.setMaxAge(60*60*24*365);//如果是-1 就是永久,单位是秒,如果不设置，cookie就不会写入硬盘，只会写入内存
         log.info("write cookieName:{}, cookieValue:{}", ck.getName(), ck.getValue());
 
